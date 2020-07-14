6aea69372919cc8e13cb3b55f43eca72d9d901a1
Neha Kumar <nehapc@neha.local>
2020-07-15 01:19:57 +0530

    Timer Javascript

---
+++ public/js/timercounter.js
@@ -0,0 +1,25 @@
+let countDownString='28 July 2020 12:00:00';
+
+// function to call timer every second 
+setInterval(function(){
+    let countDownDate = new Date (countDownString).getTime()/1000;
+    // dividing to convert  millisecondsto seconds 
+    let now=new Date().getTime()/1000;
+    // the difference is calculated in seconds 
+    let diff = Math.floor(countDownDate - now);
+    // console.log(diff); 
+    // convert to days from seconds 
+    let days=Math.floor(diff/(60*60*24));
+    //convert seconds to hours 
+    let hours = Math.floor((diff % (60*60*24)) /(60*60));
+    //convert seconds to mins
+    let mins = Math.floor((diff % (60*60))/60);
+    let secs = Math.floor(diff % 60);
+    if(diff > -1)
+    {
+        document.getElementById('days').innerText=days;
+        document.getElementById('hours').innerText=hours;
+        document.getElementById('mins').innerText=mins;
+        document.getElementById('secs').innerText=secs;
+    }
+},1000);
