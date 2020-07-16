6efa4a8fbf782212db910421d60a27962f27325a
Neha Kumar <nehapc@neha.local>
2020-07-16 22:30:18 +0530

    Home and signup route

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 // requiring modules 
 const express = require('express');
-const mysql      = require('mysql');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 // setting up the app constant which will make use of express module 
 const app = express();
 // setting the view engine of ejs 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 //globally used variables
 let locationRecommendations=[];
-
-
+let userExists = false;
 //setting up database connection
 const connection = mysql.createConnection({
   host     : 'localhost',
@@ -49,11 +50,46 @@ app.get('/', (req, res) => {
 });
 
 // handling route for login 
-
+// app.post('/login',(req,res) => {
+// const mail = req.body.
+// });
 //handling route for sign-up
 app.get('/signup',(req,res) => {
 res.render('signup');
 });
+app.post('/signup',(req,res) => {
+  //storing the values for user sign up
+  const fname = req.body.userfname;
+  const lname = req.body.userlname;
+  const phn = req.body.userphn;
+  const mail = req.body.userEmail;
+  const password = req.body.userpass;
+  const bday = req.body.userbday;
+  userExists=false;
+  let check = 'SELECT *FROM customer_data WHERE EMAIL_ID ='+ mysql.escape(mail);
+  connection.query(check,(error,results) => {
+  if(!error){
+    if(results.length == 0){
+      let register = 'INSERT INTO customer_data SET ?';
+      connection.query(register,{EMAIL_ID:mail,FIRST_NAME:fname,LAST_NAME:lname,PHONE_NO:phn,BIRTHDAY:bday},(error,results)=> {
+        if(!error){
+          let saveInfo = 'INSERT INTO login_data SET ?';
+          connection.query(saveInfo,{EMAIL_ID:mail,PASSWORD:password},(error,results)=>{
+            res.redirect('/profile');
+          })
+        }
+      })
+    }else{
+      userExists = true;
+      res.redirect('/signup');
+    }
+  }
+  })
+});
 
+//handling route for profile
+app.get('/profile',(req,res) => {
+res.render('profile');
+});
 //app listening on port 4000
 app.listen(4000, () => console.log('App listening on port 4000!'));
